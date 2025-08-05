bfdac858964965f0cc0f2eb7f888a1c2255134b6
Super User <wwyxhqc1@foxmail.com>
2025-08-05 17:10:36 +0800

    App.vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <ParticlesBackground />
+    <div class="container">
+      <LeftPanel />
+      <LoginPanel />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import LeftPanel from './components/LeftPanel.vue'
+import LoginPanel from './components/LoginPanel.vue'
+import ParticlesBackground from './components/ParticlesBackground.vue'
+</script>
