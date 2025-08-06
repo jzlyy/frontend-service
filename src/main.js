8afde067d124b6ef9c4dd4dfd639cacb9b362daa
Fucheng Zhu <wwyxhqc1@foxmail.com>
2025-08-06 17:17:09 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
+import router from './router' // 引入路由
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router) // 使用路由
+    .mount('#app')
