bcf6728e7fc377737ed56042bb34f8b24a365eee
Fucheng Zhu <wwyxhqc1@foxmail.com>
2025-08-06 17:17:28 +0800

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,12 @@
     <ParticlesBackground />
     <div class="container">
       <LeftPanel />
-      <LoginPanel />
+      <router-view /> <!-- 路由出口，替换原有的 LoginPanel -->
     </div>
   </div>
 </template>
 
 <script setup>
 import LeftPanel from './components/LeftPanel.vue'
-import LoginPanel from './components/LoginPanel.vue'
 import ParticlesBackground from './components/ParticlesBackground.vue'
 </script>
